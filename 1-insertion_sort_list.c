5f8aa30d809e459ed9f45f28b4f5a0adf5f01a8a
ghofranaaa <smailikhaled841@gmail.com>
2024-04-13 23:06:30 +0100

    sorting a doubly linked list using insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *prv_n;
 	listint_t *current;
 
-if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
